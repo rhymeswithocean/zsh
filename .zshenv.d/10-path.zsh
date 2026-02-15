189b97a935973e5e9a1012ade1cb404b43757a65
Roshan Studley <iluminated.horse@gmail.com>
2026-02-15 14:55:51 -0800

    move some env-vars

--- .zshenv.d/10-path.zsh
+++ .zshenv.d/10-path.zsh
@@ -1,6 +1,6 @@
 typeset -U path
 
-# Define the directories you want to prepend
+# Define the directories you want to apppend
 local append_path=(
   $HOME/bin
   $HOME/sbin
@@ -17,7 +17,7 @@ local append_path=(
 # Prepend custom directories (existing ones will be moved to front due to -U)
 for dir in ${(Oa)append_path}; do
   if [[ -d $dir ]]; then
-    path=($dir $path)
+    path=($path $dir)
   fi
 done
 
